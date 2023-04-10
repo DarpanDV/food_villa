671c16582c4ee6dc6e46c41850fcb8a6067444d0
DarpanDV <varshdarpan@gmail.com>
2023-04-11 01:05:28 +0530

    added the search functionality and also real world api

--- src/Components/ResCard.js
+++ src/Components/ResCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { IMG_CDN_URL } from '../Config'
 
 const ResCard = ({cloudinaryImageId,name,cuisines,avgRating}) => {
   return (
     <div className='rescard-main'>
       <div>
-      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+cloudinaryImageId} alt='error'/>
+      <img src={IMG_CDN_URL+cloudinaryImageId} alt='error'/>
       <h1>{name}</h1>
       <h2>{cuisines.join(", ")}</h2>
       <h3>{avgRating}</h3>
---
+++ src/Config.js
@@ -0,0 +1,620 @@
+ export const IMG_CDN_URL="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+
+ export const reslist = [
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "5934",
+        "name": "Burger King",
+        "uuid": "ac56f2ac-71d3-4978-9513-b3aa68e25463",
+        "city": "1",
+        "area": "Koramangala",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "28fb13049b4e55297bb3f703cde63c35",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "tags": [
+  
+        ],
+        "costForTwo": 35000,
+        "costForTwoString": "₹350 FOR TWO",
+        "deliveryTime": 23,
+        "minDeliveryTime": 23,
+        "maxDeliveryTime": 23,
+        "slaString": "23 MINS",
+        "lastMileTravel": 1.600000023841858,
+        "slugs": {
+          "restaurant": "burger-king-koramangala",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "Unit No. 1, Davar Atrium, Corporation No. 118, Koramangala Industrial Area 7th Block, Bangalore 560095",
+        "locality": "7th Block",
+        "parentId": 166,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+  
+        ],
+        "aggregatedDiscountInfo": {
+          "header": "Get every item under 129",
+          "shortDescriptionList": [
+            {
+              "meta": "Get every item under 129",
+              "discountType": "FinalPrice",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "Get every item under 129",
+              "discountType": "FinalPrice",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "",
+          "shortDescriptionList": [
+            {
+              "meta": "",
+              "discountType": "FinalPrice",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "Get every item under 129",
+              "discountType": "FinalPrice",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [
+  
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3100,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3100,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3100",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=6435858~p=1~eid=00000187-6718-3817-2194-971300aa0113",
+        "badges": {
+          "imageBased": [
+  
+          ],
+          "textBased": [
+  
+          ],
+          "textExtendedBadges": [
+  
+          ]
+        },
+        "lastMileTravelString": "1.6 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "5934",
+          "deliveryTime": 23,
+          "minDeliveryTime": 23,
+          "maxDeliveryTime": 23,
+          "lastMileTravel": 1.600000023841858,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.2",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "229",
+        "name": "Meghana Foods",
+        "uuid": "4fdd19e2-5d0f-4bde-9c7f-dc3e8d36021f",
+        "city": "1",
+        "area": "Koramangala",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "xqwpuhgnsaf18te7zvtv",
+        "cuisines": [
+          "Biryani",
+          "Andhra",
+          "South Indian",
+          "North Indian",
+          "Chinese",
+          "Seafood"
+        ],
+        "tags": [
+  
+        ],
+        "costForTwo": 50000,
+        "costForTwoString": "₹500 FOR TWO",
+        "deliveryTime": 18,
+        "minDeliveryTime": 18,
+        "maxDeliveryTime": 18,
+        "slaString": "18 MINS",
+        "lastMileTravel": 1.2999999523162842,
+        "slugs": {
+          "restaurant": "meghana-foods-5th-block-koramangala",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "124, Near Jyothi Nivas College, 1st Cross, KHB Colony, Koramangala 5th Block, Bangalore",
+        "locality": "5th Block",
+        "parentId": 635,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+  
+        ],
+        "aggregatedDiscountInfo": {
+          "header": "60% off",
+          "shortDescriptionList": [
+            {
+              "meta": "60% off | Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "60% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+  
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "distance",
+              "fee": 3100,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3100,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3100",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [
+  
+          ],
+          "textBased": [
+  
+          ],
+          "textExtendedBadges": [
+  
+          ]
+        },
+        "lastMileTravelString": "1.2 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "229",
+          "deliveryTime": 18,
+          "minDeliveryTime": 18,
+          "maxDeliveryTime": 18,
+          "lastMileTravel": 1.2999999523162842,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "4.3",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "425",
+        "name": "Hotel Empire",
+        "uuid": "c0c37758-2e83-4429-aad6-eb94debb48f5",
+        "city": "1",
+        "area": "Koramangala",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "un4omn7rcunkmlw6vikr",
+        "cuisines": [
+          "North Indian",
+          "Kebabs",
+          "Biryani"
+        ],
+        "tags": [
+  
+        ],
+        "costForTwo": 45000,
+        "costForTwoString": "₹450 FOR TWO",
+        "deliveryTime": 29,
+        "minDeliveryTime": 29,
+        "maxDeliveryTime": 29,
+        "slaString": "29 MINS",
+        "lastMileTravel": 1.2999999523162842,
+        "slugs": {
+          "restaurant": "hotel-empire-5th-block-koramangala",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "103, Industrial Area, 5th Block, Near Jyothi Nivas College, Koramangala 5th Block, Bangalore",
+        "locality": "Koramangala",
+        "parentId": 475,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+  
+        ],
+        "aggregatedDiscountInfo": {
+          "header": "60% off",
+          "shortDescriptionList": [
+            {
+              "meta": "60% off | Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "60% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [
+  
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3100,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3100,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3100",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=6428800~p=10~eid=00000187-6718-3817-2194-971600aa0a1b",
+        "badges": {
+          "imageBased": [
+  
+          ],
+          "textBased": [
+  
+          ],
+          "textExtendedBadges": [
+  
+          ]
+        },
+        "lastMileTravelString": "1.2 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "425",
+          "deliveryTime": 29,
+          "minDeliveryTime": 29,
+          "maxDeliveryTime": 29,
+          "lastMileTravel": 1.2999999523162842,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.1",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "121603",
+        "name": "Kannur Food Point",
+        "uuid": "51983905-e698-4e31-b0d7-e376eca56320",
+        "city": "1",
+        "area": "Tavarekere",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "bmwn4n4bn6n1tcpc8x2h",
+        "cuisines": [
+          "Kerala",
+          "Chinese"
+        ],
+        "tags": [
+  
+        ],
+        "costForTwo": 30000,
+        "costForTwoString": "₹300 FOR TWO",
+        "deliveryTime": 20,
+        "minDeliveryTime": 20,
+        "maxDeliveryTime": 20,
+        "slaString": "20 MINS",
+        "lastMileTravel": 3,
+        "slugs": {
+          "restaurant": "kannur-food-point-btm",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "6/21,9TH CROSS ,1ST MAIN, VENKATESHWARA LAYOUT,SG PALYA, BENGALURU, - 560093",
+        "locality": "SG Palya",
+        "parentId": 20974,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [
+  
+        ],
+        "aggregatedDiscountInfo": {
+          "header": "60% off",
+          "shortDescriptionList": [
+            {
+              "meta": "60% off | Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "60% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹100 | Use code WELCOMEBACK",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [
+  
+        ],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "distance",
+              "fee": 3900,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3900,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3900",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [
+  
+          ],
+          "textBased": [
+  
+          ],
+          "textExtendedBadges": [
+  
+          ]
+        },
+        "lastMileTravelString": "3 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "121603",
+          "deliveryTime": 20,
+          "minDeliveryTime": 20,
+          "maxDeliveryTime": 20,
+          "lastMileTravel": 3,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "3.9",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+  ]
+  
